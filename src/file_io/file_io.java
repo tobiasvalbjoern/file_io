ee324c23a0884bfe921f851b68dff8dce52ea6f6
tobias <tobias@tobias-Latitude-E6430>
2018-09-03 08:32:43 +0200

    A function for fileinput, reversecase, and fileoutput has been created.

--- src/file_io/file_io.java
+++ src/file_io/file_io.java
@@ -1,46 +1,72 @@
 package file_io;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.io.FileWriter;
 /**
  *
  * @author tobias
  */
-import java.io.*;
+//import java.io.*;
 
 public class file_io {
 
-    public static void main(String[] args) throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
-
-        try {
-            in = new FileInputStream("input.txt");
+    public static void main(String[] args) throws IOException {     
+            String path_in="/home/tobias/eclipse-workspace/file_io/input.txt";
+            String str=r_file_to_string(path_in);
+                        
+            str=reverseCase(str);
             
-            //true means appending. If you want to overwrite
-            //remove the second argument.  
-            out = new FileOutputStream("output.txt",true);
-            reverseCase(in,out);
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
-        }
+            String path_out="/home/tobias/eclipse-workspace/file_io/output.txt";
+            w_string_to_file(path_out,str); 
+        } 
+            
+    private static String r_file_to_string(String filePath){
+    	StringBuilder contentBuilder= new StringBuilder();
+    	try(Stream<String> stream = Files.lines(Paths.get(filePath),StandardCharsets.UTF_8)){
+    		stream.forEach(s-> contentBuilder.append(s).append("\n"));
+    	}
+    	catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	return contentBuilder.toString();
     }
-
-    private static void reverseCase(FileInputStream t_in, FileOutputStream t_out) throws IOException {
-    	int c;
+    
+    private static void w_string_to_file(String filename, String input_buffer) {
+    	try {
+			File file = new File("output.txt");
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write(input_buffer);
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
         
-        while ((c = t_in.read()) != -1) {
-        	if (Character.isUpperCase(c)) {
-                c = Character.toLowerCase(c);
-            } else if (Character.isLowerCase(c)) {
-                c = Character.toUpperCase(c);
-            }       
-        t_out.write(c);
-        
-        }
+    private static String reverseCase(String t_str) {
+       	int offset = 0, strLen = t_str.length();
+    	String str="";
+    	while (offset < strLen-1) {
+    	  int cur_char = t_str.codePointAt(offset);
+    	  offset += Character.charCount(cur_char);
+    	   
+    	  char c='b';
+    	 
+    	  if(Character.isUpperCase(t_str.charAt(offset-1))){
+    		  c = Character.toLowerCase(t_str.charAt(offset-1));
+    	  }
+    	  else if (Character.isLowerCase(t_str.charAt(offset-1))) {
+              c = Character.toUpperCase(t_str.charAt(offset-1));
+          }       
+    	 String temp=Character.toString(c);
+    	 str+=temp;
+    	}
+    	str+="\n";
+    	return str;
     }
 }
-
